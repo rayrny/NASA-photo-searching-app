41bbc87e8efcd8c1d24db477ee1970ac87416dce
Ray <bion239@naver.com>
2021-08-06 20:12:32 +0900

    [BE] test: axios쪽 page 파라미터 추가

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-export function searchPhoto(searchQuery) {
-	return axios.get('https://images-api.nasa.gov/search?q=' + searchQuery);
+export function searchPhoto(searchQuery, page) {
+	return axios.get('https://images-api.nasa.gov/search?q=' + searchQuery + '&page=' + page);
 }
